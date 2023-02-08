df27d425dd3d1846f3b863f15b8e7f5e503c4a73
Hoang-Trung-Le <hoang.t.le@student.uts.edu.au>
2023-02-08 09:19:02 +0700

    dashboard update 8/2

---
+++ Mockup/leaflet.js
@@ -0,0 +1,116 @@
+var map = L.map('mapid').setView([-33.8735670, 151.2068498], 10); // ([coordinates], zoom scale)
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var StamenTerrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+});
+
+var StadiaAlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+});
+
+coords = [[-33.88505, 151.04390], [-33.91977, 150.92550], [-33.7512, 150.6941], [-32.5421803, 151.2185641], [-33.8139, 151.0044]];
+stationNames = ['Lidcombe', 'Liverpool', 'Penrith', 'Hunter Valley', 'Parramatta'];
+ppb25 = [5, 10, 8, 12, 15];
+ppb10 = [7, 9, 3, 20, 10];
+
+let l = coords.length;
+
+var defaultIcon = L.icon({
+	iconUrl: '/images/marker2.svg',
+	iconSize: [50, 50],
+	// iconAnchor: [15, 46],
+	popupAnchor: [12, -34]
+});
+
+var largeIcon = L.icon({
+	iconUrl: '/images/marker1.svg',
+	iconSize: [60, 60],
+	// iconAnchor: [15, 46],
+	popupAnchor: [12, -34]
+});
+
+for (let i = 0; i < l; i++) {
+	var pop = L.popup({
+		closeOnClick: true
+	}).setContent('<h4>Station: ' + stationNames[i] + '<br>PM2.5: ' + ppb25[i] + '<br> PM10: ' + ppb10[i] + '</h4>');
+
+	// var marker = L.marker(coords[i]).addTo(map).bindPopup(pop);
+
+	(function (marker) {
+		marker.on('mouseover', function () {
+		  marker.setIcon(largeIcon);
+		});
+	
+		marker.on('mouseout', function () {
+		  marker.setIcon(defaultIcon);
+		});
+	  })(L.marker(coords[i], {icon: defaultIcon}).addTo(map).bindPopup(pop));
+
+	// marker.on('mouseover', function () {
+	// 	marker.setIcon(largeIcon);
+	// });
+
+	// marker.on('mouseout', function () {
+	// 	marker.setIcon(defaultIcon);
+	// });
+
+	// var tooltip = L.tooltip({
+	// 	permanent: true
+	// }).setContent(stationNames[i]);
+	// marker.bindTooltip(tooltip);
+}
+
+// for (var i = 0; i < markers.length; i++) {
+// 	(function (marker) {
+// 		marker.on('mouseover', function () {
+// 		  marker.setIcon(largeIcon);
+// 		});
+	
+// 		marker.on('mouseout', function () {
+// 		  marker.setIcon(defaultIcon);
+// 		});
+// 	  })(L.marker(markers[i], {icon: defaultIcon}).addTo(map));
+//   }
+
+
+var baseMaps = {
+	'Open Street Map': osm,
+	'Terrain': StamenTerrain,
+	'Dark Mode': StadiaAlidadeSmoothDark
+}
+
+L.control.layers(baseMaps).addTo(map);
+// map.attributionControl.setPrefix('')
+
+// var markers = [
+// 	[-33.88505, 151.04390],
+// 	[-33.91977, 150.92550],
+// 	[-33.7512, 150.6941]
+//   ];
+  
+//   var defaultIcon = L.icon({
+// 	iconUrl: '/images/nsw-logo.svg',
+// 	iconSize: [25, 41],
+// 	iconAnchor: [12, 41],
+// 	popupAnchor: [1, -34]
+//   });
+  
+//   var largeIcon = L.icon({
+// 	iconUrl: '/images/nsw-logo.svg',
+// 	iconSize: [50, 82],
+// 	iconAnchor: [25, 82],
+// 	popupAnchor: [1, -34]
+//   });
+  
+
+  
